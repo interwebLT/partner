92cc0ae27a793a72680fa1f05ad3663be0944a38
jmendoza46 <jose.mari.mendoza@gmail.com>
2015-12-07 16:14:59 +0800

    Externalized checkout api endpoint and added error handlers when fetching tokens

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -6,22 +6,29 @@ class CheckoutController < SecureController
 	end
 	
 	def payment_token
-		payload = payment_token_payload params[:value]
+    payload = payment_token_payload params[:value]
 		
-		response = fetch_token payload
-		json = response.body
-		hash = JSON.parse json
+    response = HTTParty.post  Rails.configuration.checkout_endpoint + "/tokens/payment",
+                              headers: checkout_headers,
+                              body: payload
+    
+    # Add additional attributes needed by Checkout form
+		hash = JSON.parse response.body
 		hash["form_authenticity_token"] = form_authenticity_token
     hash["partner_id"] = current_user.id
     hash["partner_email"] = current_user.email
+
 		render :status => response.code, :json => hash.to_json
   rescue => e
 		render :status => :internal_server_error, :json => {"error" => "Failed to fetch payment token. Returned: #{e}"}
 	end
 	
 	def verify
-		response = verify_charge params[:token]
-		render :status => response.code, :json => response.body
+    response = HTTParty.post  Rails.configuration.checkout_endpoint + "/charges/#{token}",
+                              headers: checkout_headers,
+                              body: payload
+
+    render :status => response.code, :json => response.body
 	rescue => e
 		render :status => :internal_server_error, :json => {"error" => "Failed to verify charge from Checkout. Returned: #{e}"}
 	end
@@ -31,17 +38,13 @@ class CheckoutController < SecureController
 	def api_key
 		Rails.configuration.checkout_sk
 	end
-	
-	def fetch_token payload
-    uri = URI.parse("https://sandbox.checkout.com/api2/v2/tokens/payment")
-    https = Net::HTTP.new uri.host, uri.port
-    https.use_ssl = true
-
-		request = Net::HTTP::Post.new uri.request_uri, initheader = {"Content-Type" => "application/json",  "Authorization" => api_key}
-		request.body = payload
-
-		response = https.request request
-	end
+  
+  def checkout_headers
+    {
+      "Content-Type" => "application/json",
+      "Authorization" => api_key
+    }
+  end
 	
 	def payment_token_payload amount
 		{
@@ -50,14 +53,4 @@ class CheckoutController < SecureController
 		}.to_json
 	end
 	
-	def verify_charge token
-		uri = URI.parse("https://sandbox.checkout.com/api2/v2/charges/#{token}")
-    https = Net::HTTP.new uri.host, uri.port
-    https.use_ssl = true
-
-		request = Net::HTTP::Get.new uri.request_uri, initheader = {"Authorization" => api_key}
-
-		response = https.request request
-	end
-
 end
--- config/initializers/checkout.rb
+++ config/initializers/checkout.rb
@@ -2,3 +2,4 @@ config = Rails.application.config_for(:checkout).with_indifferent_access
 
 Rails.configuration.checkout_sk = config[:secret_key]
 Rails.configuration.checkout_pk = config[:public_key]
+Rails.configuration.checkout_endpoint = config[:endpoint]
