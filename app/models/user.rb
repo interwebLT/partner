c5baa7153dc752935c238490b6c5e009535552df
Miguel Arguelles <cmarguel@dot.ph>
2015-12-14 14:45:11 +0800

    Fix User for compatibility with new User model in registry

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User
 
   devise :api_authenticatable
 
-  attr_accessor :id, :username, :password, :token, :partner_id, :partner_name, :credits, :transactions_allowed, :admin, :staff, :email
+  attr_accessor :id, :username, :password, :token, :email, :name, :salt, :encrypted_password, :registered_at, :partner_id, :partner_name, :credits, :transactions_allowed, :admin, :staff
 
   def self.url id = nil
     super id: id, resource: 'user'
