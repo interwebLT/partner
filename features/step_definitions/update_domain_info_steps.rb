f8597497fb76ead85dfab98d6a5051a96cb55abc
Ealden Esto E. Escañan <ealden@dot.ph>
2016-02-12 18:14:06 +0800

    Load updated Domain info after updating contact

--- features/step_definitions/update_domain_info_steps.rb
+++ features/step_definitions/update_domain_info_steps.rb
@@ -9,9 +9,14 @@ When /^I update the registrant of a domain$/ do
   stub_patch  to: Contact.url(id: 'registrant'),
               returns: 'contacts/registrant/patch_response'.json
 
+  stub_get  to: Domain.url(id: 1),
+            returns: 'domains/1/get_after_update_response'.json
+
+  site.domain_info.registrant.local_name.set 'Updated'
+
   site.domain_info.registrant.submit.click
 end
 
 Then /^domain registrant must be updated$/ do
-  expect(site.domain_info.registrant.local_name.value).to eql 'Registrant'
+  expect(site.domain_info.registrant.local_name.value).to eql 'Updated'
 end
