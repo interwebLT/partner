bffa456e7cd85f791827a537fc06c5c822be93d3
Ealden Esto E. Escañan <ealden@dot.ph>
2016-02-12 15:41:44 +0800

    Load domain info page

--- features/step_definitions/update_domain_info_steps.rb
+++ features/step_definitions/update_domain_info_steps.rb
@@ -1,5 +1,8 @@
 When /^I update the registrant of a domain$/ do
-  pending # Write code here that turns the phrase above into concrete actions
+  stub_get  to: Domain.url(id: 1),
+            returns: 'domains/1/get_response'.json
+
+  site.domain_info.load(id: 1)
 end
 
 Then /^domain registrant must be updated$/ do
