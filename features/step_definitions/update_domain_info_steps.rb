ccf119ab1a8deaf5ac1480c4ada9b37b4cf0579d
Ealden Esto E. Escañan <ealden@dot.ph>
2016-02-12 16:20:46 +0800

    Split loading of Domain Info page from updating it

--- features/step_definitions/update_domain_info_steps.rb
+++ features/step_definitions/update_domain_info_steps.rb
@@ -1,9 +1,11 @@
-When /^I update the registrant of a domain$/ do
+Given /^I am viewing a domain$/ do
   stub_get  to: Domain.url(id: 1),
             returns: 'domains/1/get_response'.json
 
   site.domain_info.load(id: 1)
+end
 
+When /^I update the registrant of a domain$/ do
   stub_patch  to: Contact.url(id: 'registrant'),
               returns: 'contacts/registrant/patch_response'.json
 
--- features/update_domain_info.feature
+++ features/update_domain_info.feature
@@ -2,5 +2,6 @@ Feature: Update Domain Info
 
   Scenario: Update Domain Registrant
     Given I am authenticated as partner
+    And   I am viewing a domain
     When  I update the registrant of a domain
     Then  domain registrant must be updated
