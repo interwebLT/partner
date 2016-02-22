d34d9c5b99b3409608d921f2849c809f89e1f3f8
Ealden Esto E. Escañan <ealden@dot.ph>
2016-02-22 16:09:46 +0800

    WIP: Register available domain

--- features/register_domain.feature
+++ features/register_domain.feature
@@ -1,18 +1,6 @@
-Feature: Register a domain
-  In order to register a domain
-  As a partner
-  I must be able to place an order for a domain
+Feature: Register Domain
 
   @wip
-  Scenario: Register Domain Completion
-    Given I am authenticated as partner
-    And the domain "domain.ph" does not exist
-    When I try to register "domain.ph"
-    Then it should create a new order
-
-  @wip
-  Scenario: Register Existing Domain
-    Given I am authenticated as partner
-    And the domain "domain.ph" exists
-    When I try to register "domain.ph"
-    Then I should get a message saying "already exists"
+  Scenario: Register available domain
+    When  I register an available domain
+    Then  domain must be registered
--- features/step_definitions/register_domain_steps.rb
+++ features/step_definitions/register_domain_steps.rb
@@ -1,32 +1,7 @@
-domain_conflict = false
-
-When(/^I try to register "(.*?)"$/) do |arg1|
-  search_domain
-
-  unless domain_conflict
-    should_be_on_create_contact_page
-
-    create_contact
-  end
+When /^I register an available domain$/  do
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
-Given(/^the domain "(.*?)" exists$/) do |arg1|
-  domain_conflict = true
-  domain_exists
-end
-
-Given(/^the domain "(.*?)" does not exist$/) do |arg1|
-  domain_does_not_exist
-end
-
-Then(/^the order should not be created$/) do
-  assert !(page.has_content? 'Order placed')
-end
-
-Then(/^I should get a message saying "(.*?)"$/) do |message|
-  assert page.has_content? 'Sorry, the domain name you entered is not available or is already taken. Please search again.'
-end
-
-Then(/^it should create a new order$/) do
-  assert page.has_content? 'Order placed'
+Then /^domain must be registered$/  do
+  pending # Write code here that turns the phrase above into concrete actions
 end
