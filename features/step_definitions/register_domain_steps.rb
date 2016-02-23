c5d2b9486abb96cf13746e375e6b7bf69bc2baef
Ealden Esto E. Escañan <ealden@dot.ph>
2016-02-23 11:09:57 +0800

    Use select instead of set for dropdowns

--- features/step_definitions/register_domain_steps.rb
+++ features/step_definitions/register_domain_steps.rb
@@ -7,13 +7,13 @@ When /^I try register an available domain$/  do
   site.register.domain_name.set 'available.ph'
   site.register.submit.click
 
-  site.register.registrant.local_name.set         'Registrant'
-  site.register.registrant.local_organization.set 'Organization'
-  site.register.registrant.local_street.set       'Street'
-  site.register.registrant.local_city.set         'City'
-  site.register.registrant.local_country_code.set 'XX'
-  site.register.registrant.voice.set              '+63.21234567'
-  site.register.registrant.email.set              'registrant@available.ph'
+  site.register.registrant.local_name.set             'Registrant'
+  site.register.registrant.local_organization.set     'Organization'
+  site.register.registrant.local_street.set           'Street'
+  site.register.registrant.local_city.set             'City'
+  site.register.registrant.local_country_code.select  'Philippines'
+  site.register.registrant.voice.set                  '+63.21234567'
+  site.register.registrant.email.set                  'registrant@available.ph'
   site.register.registrant.submit.click
 end
 
