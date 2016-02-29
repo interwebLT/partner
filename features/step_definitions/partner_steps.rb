35fa1dbd04359aa38ca4ad18fd666390370be6e3
Ealden Esto E. Escañan <ealden@dot.ph>
2016-02-29 14:12:41 +0800

    Use feature assets for View Partners

--- features/step_definitions/partner_steps.rb
+++ features/step_definitions/partner_steps.rb
@@ -1,15 +1,24 @@
 When /^I try to view a list of all partners$/ do
-  view_partners
+  stub_get to: Partner.url, returns: 'partners/get_response'.json
+
+  site.partners.load
 end
 
 When /^I try to view the info of a partner$/ do
-  view_partner_info
+  stub_get to: Partner.url(id: 1), returns: 'partners/1/get_response'.json
+
+  site.partner_info.load id: 1
 end
 
 Then /^I must see a list of all partners$/ do
-  assert_partners_displayed
+  expect(site.partners).to be_displayed
+
+  expect(site.partners.partners.count).to eql 3
 end
 
 Then /^I must see the info of the partner$/ do
-  assert_partner_info_displayed
+  expect(site.partner_info).to be_displayed
+
+  expect(site.partner_info.default_nameservers.count).to eq 2
+  expect(site.partner_info.pricing.count).to eq 12
 end
