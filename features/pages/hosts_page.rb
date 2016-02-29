ea7e89cbf093fce3e5475f08f216761211dd5ec5
Ealden Esto E. Escañan <ealden@dot.ph>
2016-02-29 15:23:54 +0800

    Use feature assets for View Hosts

--- features/pages/hosts_page.rb
+++ features/pages/hosts_page.rb
@@ -1,31 +1,7 @@
 class HostsPage < SitePrism::Page
   set_url '/hosts'
-  set_url_matcher /\/hosts$/
-
-  elements :hosts, '#hosts tbody tr'
 
   section :header, HeaderSection, 'header'
-end
 
-def view_hosts
-  stub_get to: Host.url, returns: hosts_response
-
-  site.hosts.load
-end
-
-def assert_hosts_displayed
-  site.hosts.displayed?.must_equal true
-
-  site.hosts.hosts.count.must_equal 3
-end
-
-private
-
-def hosts_response
-  [{:id=>1, :partner=>"admin", :name=>"ns3.domains.ph", :host_addresses=>[], :created_at=>"2015-01-01T00:00:00Z", :updated_at=>"2015-01-01T00:00:00Z"}, 
-  {:id=>2, :partner=>"admin", :name=>"ns4.domains.ph", :host_addresses=>[], :created_at=>"2015-01-01T00:00:00Z", :updated_at=>"2015-01-01T00:00:00Z"}, 
-  {:id=>3, :partner=>"alpha", :name=>"ns5.domains.ph", :host_addresses=>[
-      {:address=>"123.456.789.001", :type=>"v4"}, 
-      {:address=>"123.456.789.002", :type=>"v4"}
-  ], :created_at=>"2015-01-01T00:00:00Z", :updated_at=>"2015-01-01T00:00:00Z"}]
+  elements :hosts, '#hosts tbody tr'
 end
--- features/step_definitions/host_steps.rb
+++ features/step_definitions/host_steps.rb
@@ -1,15 +1,23 @@
-When(/^I try to view hosts$/) do
-  view_hosts
+When /^I try to view hosts$/ do
+  stub_get to: Host.url, returns: 'hosts/get_response'.json
+
+  site.hosts.load
 end
 
-Then(/^I must see all hosts$/) do
-  assert_hosts_displayed
+Then /^I must see all hosts$/ do
+  expect(site.hosts).to be_displayed
+
+  expect(site.hosts.hosts.count).to eql 3
 end
 
-When(/^I try to view the info of a host$/) do
-  view_host_info
+When /^I try to view the info of a host$/ do
+  stub_get to: Host.url(id: 1), returns: 'hosts/1/get_response'.json
+
+  site.host_info.load id: 1
 end
 
-Then(/^I must see the info of the host$/) do
-  assert_host_info_displayed
+Then /^I must see the info of the host$/ do
+  expect(site.host_info).to be_displayed
+
+  expect(site.host_info.host_addresses.count).to eql 2
 end
