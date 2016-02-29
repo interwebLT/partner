2682d03163464a05cdfe8c236bd3763824a6255d
Ealden Esto E. Escañan <ealden@dot.ph>
2016-02-29 14:28:40 +0800

    Use feature assets for View Activities

--- features/step_definitions/activity_steps.rb
+++ features/step_definitions/activity_steps.rb
@@ -1,7 +1,14 @@
 When /^I try to view all activities$/ do
-  view_activities
+  stub_get to: ObjectActivity.url, returns: 'activities/get_response'.json
+
+  site.activities.load
 end
 
 Then /^I must see all activities$/ do
-  assert_activities_displayed
+  expect(site.activities).to be_displayed
+
+  expect(site.activities.header).to have_menu_activities
+  expect(site.activities.header).to have_banner_title
+
+  expect(site.activities.activities.count).to eql 2
 end
