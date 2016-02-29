a0fd46a193260361f35b5522512882bb45feab1f
Ealden Esto E. Escañan <ealden@dot.ph>
2016-02-29 15:45:20 +0800

    Remove Minitest-Spec from our World

--- features/support/world.rb
+++ features/support/world.rb
@@ -1,10 +1,4 @@
 require 'capybara'
 require 'capybara/cucumber'
 require 'site_prism'
-
-require 'minitest/spec'
-
-World(MiniTest::Assertions)
-MiniTest::Spec.new(nil)
-
 require 'webmock/cucumber'
