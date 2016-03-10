8cb297d303397be8e7a1d6a0682ad9f29c706787
Ealden Esto E. Escañan <ealden@dot.ph>
2016-03-10 18:33:12 +0800

    RegistrationForm#save

--- app/models/registration_form.rb
+++ app/models/registration_form.rb
@@ -18,4 +18,32 @@ class RegistrationForm
   def method_missing method, *args
     self.registrant.send method, *args if self.registrant.respond_to? method
   end
+
+  def save token:
+    if self.registrant.save token: token
+      order = Order.new create_order_request
+
+      order.save token: token
+    else
+      false
+    end
+  end
+
+  private
+
+  def create_order_request
+    {
+      currency_code: 'USD',
+      ordered_at: DateTime.now.iso8601,
+      order_details: [
+        {
+          type: 'domain_create',
+          domain:   self.domain_name,
+          authcode: 'ABC123',
+          period:   self.period.to_i,
+          registrant_handle:  self.registrant.handle
+        }
+      ]
+    }
+  end
 end
