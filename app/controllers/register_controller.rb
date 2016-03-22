020a4d045209081555383c9aac5142e7fc715e4e
Ealden Esto E. Escañan <ealden@dot.ph>
2016-03-22 14:52:22 +0800

    Refactored controller to make more use of RegistrationForm

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -7,12 +7,9 @@ class RegisterController < SecureController
   def search
     domain_name = params[:domain_name].downcase
 
-    unless Domain.valid? domain_name
+    if !Domain.valid? domain_name
       redirect_to register_path, alert: 'Domain Not Valid'
-      return
-    end
-
-    unless Domain.exists? domain_name, token: current_user.token
+    elsif !Domain.exists? domain_name, token: auth_token
       redirect_to action: :details, domain_name: domain_name
     else
       redirect_to register_path, alert: 'Domain Not Available'
@@ -20,21 +17,13 @@ class RegisterController < SecureController
   end
 
   def details
-    domain_name = params[:domain_name]
-    period      = params[:period]
-    handle      = params[:handle]
+    @registration = RegistrationForm.new request_params
+    @registration.registrant = registrant if @registration.handle
 
-    if Domain.valid?(domain_name) and !Domain.exists?(domain_name, token: auth_token)
-      @registration = RegistrationForm.new domain_name: domain_name
-      @partner      = current_user.partner
-
-      @registration.period = period unless period.blank?
-
-      unless handle.blank?
-        registrant = Contact.find handle, token: auth_token
-
-        @registration.registrant = registrant if registrant
-      end
+    if !Domain.valid? @registration.domain_name
+      redirect_to register_path
+    elsif !Domain.exists? @registration.domain_name, token: auth_token
+      @partner = current_user.partner
     else
       redirect_to register_path
     end
@@ -55,10 +44,8 @@ class RegisterController < SecureController
   end
 
   def summary
-    @registration = RegistrationForm.new
-    @registration.domain_name = params[:domain_name]
-    @registration.period      = params[:period]
-    @registration.registrant  = Contact.find params[:handle], token: auth_token
+    @registration = RegistrationForm.new request_params
+    @registration.registrant = registrant if @registration.handle
   end
 
   def create_order
@@ -73,14 +60,20 @@ class RegisterController < SecureController
 
   private
 
-  REGISTRATION_FORM_FIELDS = [
-    :domain_name, :period, :handle,
-    :local_name, :local_organization, :local_street, :local_street2, :local_street3,
-    :local_city, :local_state, :local_postal_code, :local_country_code,
-    :voice, :voice_ext, :fax, :fax_ext, :email
-  ]
-
   def registration_params
-    params.require(:registration_form).permit(REGISTRATION_FORM_FIELDS)
+    params.require(:registration_form).permit :domain_name, :period, :handle,
+                                              :local_name, :local_organization, :local_street, :local_street2, :local_street3,
+                                              :local_city, :local_state, :local_postal_code, :local_country_code,
+                                              :voice, :voice_ext, :fax, :fax_ext, :email
+  end
+
+  def request_params
+    params.permit :domain_name, :period, :handle
+  end
+
+  def registrant
+    registrant = Contact.find params[:handle], token: auth_token
+
+    registrant.nil? ? Contact.new : registrant
   end
 end
