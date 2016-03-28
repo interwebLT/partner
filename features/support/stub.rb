686365ea10c68620424b9e55645cc33d960559b5
Ealden Esto E. Escañan <ealden@dot.ph>
2016-03-28 14:28:29 +0800

    Require 'with' when stubbing PATCH requests

--- features/support/stub.rb
+++ features/support/stub.rb
@@ -11,12 +11,6 @@ end
 def stub_post to:, returns:, with: nil
   request = stub_request :post, to
 
-  default_headers = {
-    'Authorization' => 'Token token=abcd123456',
-    'Content-Type'  => 'application/json',
-    'Accept'        => 'application/json'
-  }
-
   request.with headers: default_headers, body: with.to_json if with
 
   if returns.is_a? Fixnum
@@ -26,6 +20,16 @@ def stub_post to:, returns:, with: nil
   end
 end
 
-def stub_patch to:, returns:
-  stub_request(:patch, to).to_return status: 200, body: returns.to_json
+def stub_patch to:, with:, returns:
+  stub_request(:patch, to)
+    .with(headers: default_headers, body: with.to_json)
+    .to_return status: 200, body: returns.to_json
+end
+
+def default_headers
+  {
+    'Authorization' => 'Token token=abcd123456',
+    'Content-Type'  => 'application/json',
+    'Accept'        => 'application/json'
+  }
 end
