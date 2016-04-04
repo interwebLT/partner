e3a4bae4d8aa2ad1b0eec2e20a3c55a6d278ee70
Joey Mendoza <jm.mendoza@dot.ph>
2016-04-04 16:39:52 +0800

    Externalized checkout script

--- config/initializers/checkout.rb
+++ config/initializers/checkout.rb
@@ -3,3 +3,4 @@ config = Rails.application.config_for(:checkout).with_indifferent_access
 Rails.configuration.checkout_sk = config[:secret_key]
 Rails.configuration.checkout_pk = config[:public_key]
 Rails.configuration.checkout_endpoint = config[:endpoint]
+Rails.configuration.checkout_script = config[:script]
