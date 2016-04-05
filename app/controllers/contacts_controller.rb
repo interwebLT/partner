8bec0129ae94eae29e3d7add1ace714b1b2deac2
Ealden Esto E. Escañan <ealden@dot.ph>
2016-04-05 18:05:58 +0800

    Edit contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,35 @@
 class ContactsController < SecureController
   def index
-    @contacts = Contact.all token: current_user.token
+    @contacts = Contact.all token: auth_token
   end
 
   def show
-    @contact = Contact.find params[:id], token: current_user.token
+    @contact = Contact.find params[:id], token: auth_token
+  end
+
+  def edit
+    @contact = Contact.find params[:id], token: auth_token
+    @domain_id = params[:d]
+  end
+
+  def update
+    @contact = Contact.new contact_params
+
+    if @contact.update token: auth_token
+      domain_id = params[:contact].delete :d
+
+      redirect_to domain_path(domain_id), notice: 'Contact was updated!'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit :handle, :local_name, :local_organization,
+                                    :local_street, :local_street2, :local_street3,
+                                    :local_city, :local_state, :local_postal_code, :local_country_code,
+                                    :voice, :voice_ext, :fax, :fax_ext, :email
   end
 end
