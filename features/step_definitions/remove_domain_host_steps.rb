5fc06dcb2df23bf4548e1ec7253af54037d4bab3
Ealden Esto E. Escañan <ealden@dot.ph>
2016-04-20 11:15:25 +0800

    Pass domain name instead of id

--- features/step_definitions/remove_domain_host_steps.rb
+++ features/step_definitions/remove_domain_host_steps.rb
@@ -1,21 +1,15 @@
 When /^I remove a domain host$/ do
-  stub_get  to: Domain.url(id: 1),
-            returns: 'domains/1/get_response'.json
+  stub_delete to: DomainHost.url('domain.ph', id: 'ns5.domains.ph'),
+              returns: 'domains/domain.ph/hosts/ns5.domains.ph/delete_response'.json
 
-  stub_delete to: DomainHost.url('domain.ph', id: 1),
-              returns: 'domains/1/hosts/1/delete_response'.json
-
-  page.driver.delete '/domains/1/hosts/1'
+  page.driver.delete '/domains/domain.ph/hosts/ns5.domains.ph'
 end
 
 When /^I remove a domain host that does not exist$/ do
-  stub_get  to: Domain.url(id: 1),
-            returns: 'domains/1/get_response'.json
-
-  stub_delete to: DomainHost.url('domain.ph', id: 1),
+  stub_delete to: DomainHost.url('domain.ph', id: 'ns5.domains.ph'),
               returns: 404
 
-  page.driver.delete '/domains/1/hosts/1'
+  page.driver.delete '/domains/domain.ph/hosts/ns5.domains.ph'
 end
 
 Then /^domain host must be removed$/ do
