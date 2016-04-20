66be5f27fe7a047ee1c296b731f8db931fec5e80
Ealden Esto E. Escañan <ealden@dot.ph>
2016-04-20 14:01:59 +0800

    Check domain_hosts in domain info page

--- features/step_definitions/remove_domain_host_steps.rb
+++ features/step_definitions/remove_domain_host_steps.rb
@@ -1,4 +1,6 @@
 When /^I remove a domain host$/ do
+  expect(site.domain_info.domain_hosts).to have_attributes count: 2
+
   stub_delete to: DomainHost.url('domain.ph', id: 'ns5.domains.ph'),
               returns: 'domains/domain.ph/hosts/ns5.domains.ph/delete_response'.json
 
@@ -6,6 +8,8 @@ When /^I remove a domain host$/ do
 end
 
 When /^I remove a domain host that does not exist$/ do
+  expect(site.domain_info.domain_hosts).to have_attributes count: 2
+
   stub_delete to: DomainHost.url('domain.ph', id: 'ns5.domains.ph'),
               returns: 404
 
