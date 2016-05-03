7953d25ac7102f11bde76423544b8b4ce519ee3e
Ealden Esto E. Escañan <ealden@dot.ph>
2016-05-03 18:03:40 +0800

    Use have_attributes

--- spec/models/domain_host_spec.rb
+++ spec/models/domain_host_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe DomainHost do
       }
     }
 
-    it 'converts from JSON to model' do
-      expect(subject.id).to eql 1
-      expect(subject.name).to eql 'ns5.domains.ph'
-      expect(subject.created_at).to eql '2015-03-04T19:00:00Z'
-      expect(subject.updated_at).to eql '2015-03-04T19:00:00Z'
+    it do
+      is_expected.to have_attributes  id:         1,
+                                      name:       'ns5.domains.ph',
+                                      created_at: '2015-03-04T19:00:00Z',
+                                      updated_at: '2015-03-04T19:00:00Z'
     end
   end
 end
