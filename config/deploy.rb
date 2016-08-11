3cbdf7d0e59e29658cca17667f2069f05edd2633
Reynante De Jesus <reynan@dot.ph>
2016-08-11 16:46:42 +0800

    update for unicorn service at startup

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,8 @@ after 'deploy:publishing', 'deploy:restart'
 
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:restart'
+    on roles :all do
+      execute "/etc/init.d/unicorn upgrade"
+    end
   end
 end
