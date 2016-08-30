12fd05dfebb45e6c02c29ae455b5da9716c52570
kian1213 <chrono0424@gmail.com>
2016-08-30 10:19:33 +0800

    fix ip address list format for glue records

--- app/helpers/domain_hosts_helper.rb
+++ app/helpers/domain_hosts_helper.rb
@@ -5,24 +5,30 @@ module DomainHostsHelper
         ip_list = JSON.parse ip_list
         ipv4_list = []
         ipv6_list = []
-
         ip_list["ipv4"].each do |ipv4|
-          ipv4_list << ipv4[1]
+          unless ipv4[1].empty?
+            ipv4_list << "#{ipv4[1]} <br/>"
+          end
         end
 
         ip_list["ipv6"].each do |ipv6|
-          ipv6_list << ipv6[1]
+          unless ipv6[1].empty?
+            ipv6_list << "#{ipv6[1]} <br/>"
+          end
         end
-        ipv4_list = "IPV4: #{ipv4_list.join(', ')}".truncate(30)
-        ipv6_list = "IPV6: #{ipv6_list.join(', ')}".truncate(30)
-
-        output = "#{ipv4_list}\n#{ipv6_list}"
+        if ipv4_list.empty? && ipv6_list.empty?
+          output = "No IP Address"
+        else
+          ipv4_list = "<b>IPV4:</b><br/>#{ipv4_list.join('')}"
+          ipv6_list = "<b>IPV6:</b><br/>#{ipv6_list.join('')}"
+          output = "#{ipv4_list}#{ipv6_list}"
+        end
       else
         output = "No IP Address"
       end
     else
       output = "No IP Address"
     end
-    return output
+    return output.html_safe
   end
 end
