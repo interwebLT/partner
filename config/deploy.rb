ee27199855026efaaf8eaf2e1db610fc48cba8fb
Joey Mendoza <jm.mendoza@dot.ph>
2016-09-09 16:54:39 +0800

    Typo in deploy.rb linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :rails_env, 'production'
 
 set :deploy_to, '/srv/partner'
 set :log_level, :info
-set :linked_files, %w{config/secrets.yml config/api.yml config/exception_notification.yml config/checkout.yml config/whois.yml config/paypal.yml config/paypal_redirect_urls.yml, config/dragon_pay.yml}
+set :linked_files, %w{config/secrets.yml config/api.yml config/exception_notification.yml config/checkout.yml config/whois.yml config/paypal.yml config/paypal_redirect_urls.yml config/dragon_pay.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets}
 
 set :rbenv_path, '$HOME/.rbenv'
