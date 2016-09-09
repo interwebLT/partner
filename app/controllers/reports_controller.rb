9ec969df691c0499addb3eeaecd8d9ce56f13905
kian1213 <chrono0424@gmail.com>
2016-09-09 14:33:50 +0800

    add reports date filter

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,18 @@ class ReportsController < SecureController
   before_action :except_admin
 
   def index
-    @credits = Credit.all token: current_user.token
-    @orders = Order.all token: current_user.token
+    if params[:month].nil? && params[:year].nil?
+      @credits = Credit.all token: current_user.token
+      @orders  = Order.all token: current_user.token
+      @date_display = Date.today
+      @month = ""
+      @year  = ""
+    else
+      @credits  = Credit.all token: current_user.token
+      @orders   = Order.all token: current_user.token
+      @date_display = Date.new params[:year].to_i, params[:month].to_i
+      @month = params[:month]
+      @year  = params[:year]
+    end
   end
 end
--- app/models/credit.rb
+++ app/models/credit.rb
@@ -2,11 +2,11 @@ class Credit
   include Api::Model
 
   attr_accessor :id, :partner_id, :partner, :credit_number, :amount, :fee, :credited_at, :created_at, :updated_at, :remarks, :type, :verification_code
-  
+
   TRANSACTION_FEE = 0.05
   PAYPAL_CODE = 'EC'
   CHECKOUT_CODE = 'pay_tok'
-  
+
   def to_json
     {
       type: type,
@@ -19,7 +19,7 @@ class Credit
       remarks: remarks
     }
   end
-  
+
   def gateway
     if self.verification_code.blank?
       'Manual'
@@ -32,5 +32,19 @@ class Credit
     end
   end
 
+  def filter_included? month, year
+    if month.empty? and year.empty?
+      return true
+    else
+      included_month = self.credited_at.to_time.strftime("%m").to_i == month.to_i
+      included_year  = self.credited_at.to_time.strftime("%Y").to_i == year.to_i
+
+      if included_month and included_year
+        return true
+      else
+        return false
+      end
+    end
+  end
 end
 
