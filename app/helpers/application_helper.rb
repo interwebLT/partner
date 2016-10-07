84c3bb31d298c0a403d6b4bf00d80ae2c1c656f6
kian1213 <chrono0424@gmail.com>
2016-10-07 16:50:13 +0800

    disable buttons if user is an epp partner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def create_display_only_button type, message, action = nil
+    render partial: "shared/display_buttons", locals: {type: type, message: message, action: action}
+  end
 end
