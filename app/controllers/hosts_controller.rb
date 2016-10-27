19a797cbe6bf62d368312616b6a3ff2ffadea0bd
kian1213 <chrono0424@gmail.com>
2016-10-27 17:04:35 +0800

    render json {} if host is not available

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -9,7 +9,11 @@ class HostsController < SecureController
 
   def get_host_address
     host = Host.search(term: params[:domain_host], token: current_user.token)
-    host_addresses = host.host_addresses
-    render json: host_addresses
+    if !host.nil? or !host.empty? or !host.blank?
+      host_addresses = host.host_addresses
+      render json: host_addresses
+    else
+      render json: {}
+    end
   end
 end
