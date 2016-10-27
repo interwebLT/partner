c0f3ca3aaffb675b58ea3a6e2bd43be8b61c8ad8
kian1213 <chrono0424@gmail.com>
2016-10-27 14:46:05 +0800

    load domain history after page load

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,11 @@
 class ActivitiesController < SecureController
   def index
-    @activities = ObjectActivity.all token: current_user.token
+    if !params[:domain_id].nil?
+      @activities = Domain.get_activities params[:domain_id], current_user.token
+    else
+      if current_user.admin
+        @activities = ObjectActivity.all token: current_user.token
+      end
+    end
   end
 end
