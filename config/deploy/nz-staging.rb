c4d3dcde4ff180f2c12d2374ae05191ed8ae8aeb
Joey Mendoza <jm.mendoza@dot.ph>
2016-11-07 19:30:51 +0800

    Renamed nz to nz-staging

---
+++ config/deploy/nz-staging.rb
@@ -0,0 +1,3 @@
+role :app, %w{deploy@nz-partner.staging.local}
+role :web, %w{nz@www.staging.local}
+set :branch, 'nz-api'
--- config/deploy/nz.rb
+++
@@ -1,3 +0,0 @@
-role :app, %w{deploy@nz-partner.production.local}
-role :web, %w{nz@www.production.local}
-set :branch, 'nz-api'
