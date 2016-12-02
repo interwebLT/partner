9d25e0688289532482fad925d3aac63462620355
kian1213 <chrono0424@gmail.com>
2016-12-02 10:37:12 +0800

    get monthly report data directly from registry and remove not used code

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,17 +3,19 @@ class ReportsController < SecureController
 
   def index
     if params[:month].nil? && params[:year].nil?
-      @credits = Credit.all token: current_user.token
-      @orders  = Order.all token: current_user.token
       @date_display = Date.today
       @month = Date.today.month.to_s
       @year  = Date.today.year.to_s
+
+      @credits  = Credit.get_for_current_month @month, @year, token: current_user.token
+      @orders   = Order.get_for_current_month @month, @year, token: current_user.token
     else
-      @credits  = Credit.all token: current_user.token
-      @orders   = Order.all token: current_user.token
       @date_display = Date.new params[:year].to_i, params[:month].to_i
       @month = params[:month]
       @year  = params[:year]
+
+      @credits  = Credit.get_for_current_month @month, @year, token: current_user.token
+      @orders   = Order.get_for_current_month @month, @year, token: current_user.token
     end
   end
 end
